d2c3cdb2e00e6dd52fb9eea2be458ced7860fd8f
oseji <jiade1233@gmail.com>
2026-04-06 06:53:21 +0100

    initiated creation of script for sender create delivery broadcast

---
+++ test/specs/sender/delivery/createDeliveryBroadcast.test.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import loginPage from "../../../pageobjects/login.page";
+import deliveryRoutePage from "../../../pageobjects/sender/deliveryFlow/deliveryRoute.page";
+import deliveryDetailsFormPage from "../../../pageobjects/sender/deliveryFlow/deliveryDetailsForm.page";
+import packingInstructionsPage from "../../../pageobjects/sender/deliveryFlow/packingInstructions.page";
+
+describe("RouTag Sender - Create Delivery Broadcast", () => {
+    it("Should create a delivery broadcast successfully", async () => {
+        await loginPage.loginUser(
+            process.env.SENDER_EMAIL!,
+            process.env.SENDER_PASSWORD!,
+        );
+
+        await deliveryRoutePage.fillDeliveryRouteDetailsAndContinue(
+            process.env.PICKUP_LOCATION!,
+            process.env.DESTINATION_LOCATION!,
+        );
+
+        await deliveryDetailsFormPage.fillDeliveryDetailsFormAndContinue(
+            process.env.PACKAGE_WEIGHT!,
+            process.env.PACKAGE_DESCRIPTION!,
+            process.env.RECEIVERS_NAME!,
+            process.env.RECEIVERS_PHONE_NUMBER!,
+            process.env.PACKAGE_VALUE!,
+        );
+
+        await packingInstructionsPage.verifyPackingInstructionsPage();
+    });
+});
